ccf163e354a3d301b99f5cde879b5ec8185737ab
jonssonmattias <44271390+jonssonmattias@users.noreply.github.com>
2018-12-16 20:31:46 +0100

    Update Controller.java

--- p6/Controller.java
+++ p6/Controller.java
@@ -3,7 +3,20 @@ package p6;
 public class Controller {
 	private Viewer viewer;
 	private Array7x7 array;
+	private Array7 leftCol;
+	private Array7 rightCol;
+	private Test2UI ui;
 	
+	public Controller(Viewer viewer, Array7x7 array) {
+		this.viewer = viewer;
+		this.array = array;
+	}
+	public Controller(Test2UI ui, Array7 rightCol, Array7 leftCol, Array7x7 array) {
+		this.ui=ui;
+		this.rightCol=rightCol;
+		this.leftCol=leftCol;
+		this.array=array;
+	}
 	/**
 	 * Reads the row of the array given 
 	 * by the parameter and displays it.
@@ -12,6 +25,7 @@ public class Controller {
 	 */
 	public void readRow(int row) {
 		Array7 a7 = array.getRow(row);
+		viewer.setArray7x7(array);
 		viewer.setRow(a7);
 	}
 	/**
@@ -22,6 +36,7 @@ public class Controller {
 	 */
 	public void readCol(int col) {
 		Array7 a7 = array.getCol(col);
+		viewer.setArray7x7(array);
 		viewer.setColum(a7);
 	}
 	
@@ -32,7 +47,8 @@ public class Controller {
 	 * @param row  in which row the array will be written
 	 * @param arr7  the array to be written
 	 */
-		public void writeRow(int row, Array7 arr7) {
+	public void writeRow(int row, Array7 arr7) {
+		array.setRow(row, arr7);
 		viewer.setRowInArray(row,arr7);
 	}
 
@@ -44,7 +60,31 @@ public class Controller {
 	 * @param arr7  the array to be written
 	 */
 	public void writeCol(int col, Array7 arr7) {
+		array.setCol(col, arr7);
 		viewer.setColumInArray(col,arr7);
 	}
-	
+	/**
+	 * Moves the array to the right
+	 * 
+	 * @param arr7 an Array7-object
+	 */
+	public void moveRight(Array7 arr7) {
+		Array7 a7 = array.shiftRight(arr7);
+		ui.setArray7x7(array);
+		ui.setColRight(a7);
+		
+	}
+	/**
+	 * Moves the array to the left
+	 * 
+	 * @param arr7 an Array7-object
+	 */
+	public void moveLeft(Array7 arr7) {
+		Array7 a7 = array.shiftLeft(arr7);
+		ui.setArray7x7(array);
+		ui.setColumnLeft(a7);
+	}
+	public void setUserInput(Test2UI test2ui) {
+		this.ui = test2ui;
+	}
 }
